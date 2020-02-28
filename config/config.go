d387f21de7efa98e6dbf6ab08a5e520b7d6fb439
Demian <haski007001@gmail.com>
2020-02-28 16:34:55 +0200

    config updated

--- config/config.go
+++ config/config.go
@@ -5,3 +5,15 @@ var Port = ":8080"
 
 // HostName on witch server will be listened.
 var HostName = "localhost"
+
+
+
+
+///////////////////////////// DataBase \\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+// DataBaseHost, hostname on witch mongoDB is hosting.
+var DataBaseHost = "192.168.99.103"
+
+// DataBasePort, port on witch mongoDB is hosting.
+var DataBasePort = ":27017"
